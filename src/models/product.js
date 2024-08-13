92d806140af63d333b8f059110c7bd63d47e8da6
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-08-13 18:50:49 +0300

    Created product model

--- src/models/product.js
+++ src/models/product.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
+    brand: {
+        type: String,
+        required: true,
+        trim: true
+    },
     name: {
         type: String,
         required: true,
@@ -12,24 +17,30 @@ const productSchema = new mongoose.Schema({
         trim: true
     },
     image: {
-        type: Buffer
+        type: Buffer,
     },
-    product_info: {
+    productInfo: {
         type: String,
         trim: true
     },
+    category: {
+        type: String,
+        trim: true,
+        required: true
+    },
     stock: {
-        type: Boolean
+        type: Boolean,
+        default: true
     }
 }, {
     timestamps: true
 })
 
-favoriteSchema.virtual('favorites', {
+/* productSchema.virtual('favorites', {
     ref: 'Favorite',
     localField: '_id',
     foreignField: 'product'
-})
+}) */    
 
 const Product = mongoose.model('Product', productSchema)
 module.exports = Product
