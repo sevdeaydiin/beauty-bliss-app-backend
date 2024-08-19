d13badee5d4116465b378d749c05cb4957cff329
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-08-19 18:52:59 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ require('dotenv').config()
 var connectDB = require('./db/mongoose')
 connectDB()
 const authRouter = require('./routers/auth')
+const productRouter = require('./routers/product')
 
 app.use(express.json())
 app.use(express.static('public'))
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4001
 
 app.use(authRouter)
+app.use(productRouter)
 
 app.listen(port, () => {
     console.log('Server is ' + port)
