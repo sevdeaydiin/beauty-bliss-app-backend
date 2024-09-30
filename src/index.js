7cc5bd7cd91b1fb33369379b803b9cb864c9b6a6
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-09-30 23:07:13 +0300

    added new routers

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ var connectDB = require('./db/mongoose')
 connectDB()
 const authRouter = require('./routers/auth')
 const productRouter = require('./routers/product')
+const basketRouter = require('./routers/basket')
+const favoriteRouter = require('./routers/favorite')
+const orderRouter = require('./routers/order')
 
 app.use(express.json())
 app.use(express.static('public'))
 const port = process.env.PORT || 4001
 
-app.use(authRouter)
-app.use(productRouter)
+app.use(authRouter);
+app.use(productRouter);
+app.use(basketRouter);
+app.use(favoriteRouter)
+app.use(orderRouter)
 
 app.listen(port, () => {
     console.log('Server is ' + port)
