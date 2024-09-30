52b5304a5ac4f9dba94e1b879b5a7425ee76a13d
Sevde Aydın <74006598+sevdeaydiin@users.noreply.github.com>
2024-09-30 23:09:16 +0300

    updated auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
     try {
 
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'twitterapi')
+        const decoded = jwt.verify(token, 'bbapi')
 
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
